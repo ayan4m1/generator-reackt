087f8ebe112978c477fefed43ffdc212b8d48142
ayan4m1 <andrew@bulletlogic.com>
2018-12-09 17:57:59 -0500

    fetch license file dynamically based on selection during prompt phase, copy skeleton package.json

--- src/app/index.js
+++ src/app/index.js
@@ -1,5 +1,10 @@
 import path from 'path';
+import { format } from 'date-fns';
+import request from 'request-promise-native';
 import spdxIdentifiers from 'spdx-license-ids';
+
+spdxIdentifiers.push('SEE LICENSE IN LICENSE');
+spdxIdentifiers.sort();
 
 // we cannot use ES6 imports on this object, as it directly exports a class to
 // module.exports - no default export nor a named export is present for us to use
@@ -22,31 +27,34 @@ export default class extends Generator {
     this.answers = await this.prompt([
       {
         type: 'input',
-        name: 'project.name',
-        message: 'Project name',
+        name: 'package.name',
+        message: 'Package name',
         default: this.appname
       },
       {
         type: 'input',
-        name: 'project.version',
-        message: 'Starting version number',
+        name: 'package.version',
+        message: 'Package version',
         default: '0.1.0'
       },
       {
         type: 'autocomplete',
-        name: 'project.license',
-        message: 'License (SPDX identifier)',
+        name: 'package.license',
+        message: 'Package license',
         source: (results, input) => {
           const pattern = new RegExp(`.*${input}.*`, 'i');
 
           return new Promise(resolve => {
             resolve(
-              spdxIdentifiers
-                .filter(identifier => pattern.test(identifier))
-                .sort()
+              spdxIdentifiers.filter(identifier => pattern.test(identifier))
             );
           });
         }
+      },
+      {
+        type: 'input',
+        name: 'package.description',
+        message: 'Package description'
       },
       {
         type: 'input',
@@ -61,7 +69,7 @@ export default class extends Generator {
         default: email()
       },
       {
-        type: 'checkbox',
+        type: 'confirm',
         name: 'flags.failOnLinter',
         message: 'Should linting errors prevent commits?',
         default: true
@@ -69,16 +77,27 @@ export default class extends Generator {
     ]);
   }
 
-  configuring() {
-    this.log('configure');
-  }
+  async writing() {
+    this.fs.copyTpl(
+      this.templatePath('package.json'),
+      this.destinationPath('package.json'),
+      this.answers
+    );
 
-  writing() {
-    this.log('write');
-    this.log(this.answers);
-  }
+    const { name, email } = this.answers.author;
+    const { license } = this.answers.package;
 
-  install() {
-    this.log('install');
+    let licenseText = 'Place your license here.\n';
+
+    if (license !== 'SEE LICENSE IN LICENSE') {
+      const rawLicense = await request(
+        `https://raw.githubusercontent.com/spdx/license-list-data/master/text/${license}.txt`
+      );
+
+      licenseText = rawLicense
+        .replace('<year>', format(new Date(), 'YYYY'))
+        .replace('<copyright holders>', `${name} <${email}>`);
+    }
+    this.fs.write('LICENSE', licenseText);
   }
 }
