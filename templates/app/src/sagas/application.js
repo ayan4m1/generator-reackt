543574fba412caf829ca24f2f07f698d2f9019ca
ayan4m1 <andrew@bulletlogic.com>
2019-05-10 18:50:17 -0400

    fix invocation of watchers in app saga

--- templates/app/src/sagas/application.js
+++ templates/app/src/sagas/application.js
@@ -5,5 +5,5 @@ export const workers = {};
 export const watchers = {};
 
 export default function* saga() {
-  yield all(Object.values(watchers));
+  yield all(Object.values(watchers).map(watcher => watcher()));
 }
