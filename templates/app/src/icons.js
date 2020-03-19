8ba0c627de2a01da3821ac23fa0b6ec9d0446aa0
ayan4m1 <andrew@bulletlogic.com>
2020-03-19 14:14:57 -0400

    fix dummy icon not existing

--- templates/app/src/icons.js
+++ templates/app/src/icons.js
@@ -1,4 +1,4 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faFontAwesome } from '@fortawesome/free-solid-svg-icons';
+import { faCircle } from '@fortawesome/free-solid-svg-icons';
 
-library.add(faFontAwesome);
+library.add(faCircle);
