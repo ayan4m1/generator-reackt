88623cbe2adc79cc8d2bbd09d161df9eecc80725
ayan4m1 <andrew@bulletlogic.com>
2021-07-30 15:54:52 -0400

    drop yeoman-generator back to 4.x

--- src/app/index.js
+++ src/app/index.js
@@ -380,7 +380,7 @@ export default class extends Generator {
     this.log(
       `Getting ready to install ${main.length} dependencies and ${dev.length} dev dependencies.`
     );
-    this.addDependencies(main);
-    this.addDevDependencies(dev);
+    this.npmInstall(main, { save: true });
+    this.npmInstall(dev, { 'save-dev': true });
   }
 }
