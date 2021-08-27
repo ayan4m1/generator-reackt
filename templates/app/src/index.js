c8d018b0953a014132b84694e1494df8f19e4df6
ayan4m1 <andrew@bulletlogic.com>
2021-08-27 15:14:13 -0400

    remove unused React import

--- templates/app/src/index.js
+++ templates/app/src/index.js
@@ -1,7 +1,6 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
-import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 <% if (flags.addRedux) { %>
