35cd3f92271442a409050a7f7aa3293ba76396a0
ayan4m1 <andrew@bulletlogic.com>
2022-01-10 13:43:47 -0500

    switch from gulp to rollup, increase min supported node version to 16

---
+++ rollup.config.js
@@ -0,0 +1,25 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import { terser } from 'rollup-plugin-terser';
+import autoExternal from 'rollup-plugin-auto-external';
+import babel from '@rollup/plugin-babel';
+import multiInput from 'rollup-plugin-multi-input';
+import eslint from '@rollup/plugin-eslint';
+
+export default {
+  input: './src/**/*.js',
+  output: {
+    dir: './generators/',
+    format: 'cjs',
+    exports: 'auto'
+  },
+  plugins: [
+    eslint(),
+    autoExternal(),
+    multiInput(),
+    nodeResolve(),
+    babel({
+      babelHelpers: 'runtime'
+    }),
+    terser()
+  ]
+};
