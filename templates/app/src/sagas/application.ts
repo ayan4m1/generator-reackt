ca6ce4c14c14126225e18116ca7a30531063c18c
ayan4m1 <andrew@bulletlogic.com>
2026-08-30 19:24:05 -0400

    feat: change extension from .js to .ts for app template files

--- templates/app/src/sagas/application.ts
+++ templates/app/src/sagas/application.ts
@@ -5,5 +5,5 @@ export const workers = {};
 export const watchers = {};
 
 export default function* saga() {
-  yield all(Object.values(watchers).map(watcher => watcher()));
+  yield all(Object.values(watchers).map((watcher) => watcher()));
 }
