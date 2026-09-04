006a77513767d2614cc3804e3a162ad7a09be2bb
ayan4m1 <andrew@bulletlogic.com>
2026-09-04 19:40:55 -0400

    fix: duplicate default export

--- templates/component/index.tsx
+++ templates/component/index.tsx
@@ -2,15 +2,12 @@
 import { connect } from 'react-redux';
 <% } %>
 
-export default function <%= component.name %>() {
-  return <h1>Hello World</h1>;
+function <%= component.name %>() {
+  return <h1><%= component.name %></h1>;
 }
 
 <% if (flags.addRedux) { %>
-export default connect(
-  null,
-  null
-)(<%= component.name %>);
+export default connect(null, null)(<%= component.name %>);
 <% } else { %>
 export default <%= component.name %>;
 <% } %>
